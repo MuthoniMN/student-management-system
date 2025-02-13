eef104136bc5247dcba5447c26f6f5c694a0b079
Mitch <ndianguimichelle@gmail.com>
2025-02-13 13:03:39 +0300

    fix: seeding 10 students per grade

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -13,15 +13,19 @@ class StudentSeeder extends Seeder
         $parents = DB::table('parents')->pluck('id')->toArray(); // Get parent IDs
 
         $students = [];
-        for ($i = 1; $i <= 15; $i++) {
-            $students[] = [
-                'studentId' => strtoupper(Str::random(8)), // Unique student ID
-                'grade_id' => rand(1, 8), // Random grade from 1 to 8
-                'name' => fake()->name(), // Random readable student name
-                'parent_id' => $parents[array_rand($parents)], // Assign a random parent
-                'created_at' => now(),
-                'updated_at' => now(),
-            ];
+        for ($i = 1; $i <= 8; $i++) {
+            $s = 1;
+            while($s <= 10){
+                $students[] = [
+                    'studentId' => strtoupper(Str::random(8)), // Unique student ID
+                    'grade_id' => $i, // Random grade from 1 to 8
+                    'name' => fake()->name(), // Random readable student name
+                    'parent_id' => $parents[array_rand($parents)], // Assign a random parent
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+                $s += 1;
+            }
         }
 
         DB::table('students')->insert($students);
