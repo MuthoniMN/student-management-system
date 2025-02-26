cbca39b982c283fdb93b1e7e0f1fa85e5e2e9ce1
Mitch <ndianguimichelle@gmail.com>
2025-02-26 14:47:06 +0300

    fix: matching function names to repository function

--- app/Interfaces/StudentRepositoryInterface.php
+++ app/Interfaces/StudentRepositoryInterface.php
@@ -1,21 +1,20 @@
 <?php
 
-namespace App;
+namespace App\Interfaces;
 use App\Models\Student;
 use App\Models\Grade;
-use App\Models\Result
+use App\Models\Result;
 
 interface StudentRepositoryInterface
 {
-    public function getAllStudents();
-    public function getStudentById(Student $student);
-    public function updateStudent(Student $student, array $attributes);
-    public function updateStudentsGrade(array $attribute, Grade $grade);
-    public function getStudentsYearlyResults(Student $student, AcademicYear $academicYear);
-    public function getStudentsSemesterResults(Student $student, Semester $semester);
-    public function createStudent(array $attributes);
-    public function deleteStudent(Student $student);
-    public function deleteManyStudents(array $attributes);
-    public function restoreStudent(Student $student);
-    public function restoreManyStudents(array $attributes);
+    public function findAll();
+    public function get(Student $student);
+    public function findById(int $id);
+    public function update(Student $student, array $attributes);
+    public function upgrade(array $attribute, Grade $grade);
+    public function create(array $attributes);
+    public function delete(Student $student);
+    public function deleteMany(array $attributes);
+    public function restore(int $id);
+    public function restoreMany(array $attributes);
 }
